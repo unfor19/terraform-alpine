237fee9b98f0495c89a2ae860558a1cd11452d3a
Meir Gabay <meir@prodops.io>
2020-09-19 16:33:05 +0300

    add pipeline

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,12 @@
 FROM alpine:3.12
 
-ARG TERRAFORM_VERSION=0.12.28
-ARG AWSCLI_VERSION=1.18.2
+ARG TERRAFORM_VERSION
+ARG AWSCLI_VERSION
 
-RUN apk --update add bash curl git openssh-client py3-pip python3
-RUN pip3 install --no-cache-dir awscli==${AWSCLI_VERSION}
-
-
+RUN apk --update add bash curl git openssh-client py3-pip python3 jq
+SHELL ["/bin/bash", "-c"]
 WORKDIR $HOME/
-RUN curl -sL https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip -o terraform.zip
-RUN unzip -qq terraform.zip && \
-    rm terraform.zip && \
-    mv terraform /usr/local/bin/terraform && \
-    chmod +x /usr/local/bin/terraform
+COPY . .
+RUN ./packages.sh
 
 CMD ["/bin/bash"]
---
+++ packages.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+## Functions
+install_awscli(){
+    if [[ -n $AWSCLI_VERSION ]]; then
+        pip3 install --no-cache-dir awscli=="${AWSCLI_VERSION}"
+    else
+        # Install latest version
+        pip3 install --no-cache-dir awscli
+    fi
+}
+
+
+get_latest_terraform_version(){
+    local org_name=$1
+    local repo_name=$2
+    curl -s https://api.github.com/repos/${org_name}/${repo_name}/releases/latest | grep "tag_name" | cut -d'v' -f2 | cut -d'"' -f1
+}
+
+
+install_terraform(){
+    local terraform_version
+
+    if [[ -n $TERRAFORM_VERSION ]]; then
+        terraform_version=$TERRAFORM_VERSION
+    else
+        # Get latest version
+        terraform_version=$(get_latest_terraform_version hashicorp terraform)
+    fi
+
+    if [[ -z $terraform_version ]]; then
+        echo "[ERROR] terraform_version is empty"
+        exit 1
+    fi
+
+    curl -sL "https://releases.hashicorp.com/terraform/${terraform_version}/terraform_${terraform_version}_linux_amd64.zip" -o terraform.zip
+    unzip -qq terraform.zip && \
+        rm terraform.zip && \
+        mv terraform /usr/local/bin/terraform && \
+        chmod +x /usr/local/bin/terraform
+}
+
+
+## Main
+install_awscli
+install_terraform
